cc10fc88ccae98a77dcec585232f85a014da254e
JuunKR <bluejun0510@naver.com>
2022-01-04 23:37:23 +0900

    first commit

---
+++ 로또의_최고_순위와_최저_순위.py
@@ -0,0 +1,10 @@
+def solution(lottos, win_nums):
+    rank = [6,6,5,4,3,2,1]
+    zeros = lottos.count(0)
+    cnt = 0
+    for num in lottos:
+        if num in win_nums:
+            cnt += 1
+    return [rank[zeros + cnt], rank[cnt]]
+
+
