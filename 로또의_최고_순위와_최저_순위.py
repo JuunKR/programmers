2f646772c6b38dab49213d0dd704e42bbe2e8069
JuunKR <bluejun0510@naver.com>
2022-01-07 23:33:25 +0900

    2022.01.07

--- 로또의_최고_순위와_최저_순위.py
+++ 로또의_최고_순위와_최저_순위.py
@@ -8,3 +8,8 @@ def solution(lottos, win_nums):
     return [rank[zeros + cnt], rank[cnt]]
 
 
+if __name__ == '__main__':
+    lottos = [44, 1, 0, 0, 31, 25]
+    win_nums = [31, 10, 45, 1, 6, 19]
+    print(solution(lottos, win_nums)) 
+    # [3, 5]
